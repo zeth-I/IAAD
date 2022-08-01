3518d1cc0dc8c6d1eff6e2b1c116d2d8c3335e87
José Lucas Cabral Braga // DevZeth <jlcbraga@outlook.com>
2022-08-01 17:24:22 -0300

    Bancos de dados

---
+++ Script_Empresa.sql
@@ -0,0 +1,117 @@
+-- Introdução ao Armazenamento e Análise de Dados (IAAD) - BSI/UFRPE
+-- Script do Banco de Dados Empresa 
+
+begin;
+drop schema if exists Empresa_JoseLucas; 
+create schema Empresa_JoseLucas;
+use Empresa_JoseLucas;
+
+-- Criando as tabelas
+create table FUNCIONARIO(
+	Pnome VARCHAR(15) NOT NULL,
+	Minicial CHAR (1),
+	Unome VARCHAR (15) NOT NULL,
+	Cpf CHAR(11) NOT NULL,
+	Datanasc DATE NOT NULL,
+	Endereco VARCHAR(40),
+	Sexo CHAR(1),
+	Salario decimal(10,2), 
+	Cpf_supervisor CHAR(11),
+	Dnr INT,
+	PRIMARY KEY(Cpf));  
+
+create table DEPARTAMENTO(
+	Dnome VARCHAR(15) NOT NULL,
+	Dnumero INT PRIMARY KEY, 
+	Cpf_gerente CHAR(11),
+	Data_inicio_gerente DATE,
+
+	UNIQUE(Dnome));
+	
+create table LOCALIZACAO_DEP(
+	Dnumero INT NOT NULL,
+	Dlocal VARCHAR(15) NOT NULL,
+	PRIMARY KEY (Dnumero, Dlocal));
+create table PROJETO(
+	Projnome VARCHAR(15) NOT NULL,
+	Projnumero INT NOT NULL,
+	Projlocal VARCHAR(15),
+	Dnum INT NOT NULL,
+	PRIMARY KEY(ProjNumero),
+	UNIQUE(Projnome));
+create table DEPENDENTE(
+	Fcpf CHAR(11) NOT NULL,
+	Nome_dependente VARCHAR(15) NOT NULL,
+	Sexo CHAR,
+	Datanasc DATE,
+	Parentesco VARCHAR(8),
+	PRIMARY KEY(Fcpf, Nome_dependente));
+create table TRABALHA_EM(
+	Fcpf CHAR (11) NOT NULL,
+	Pnr INT  NOT NULL,
+	Horas DECIMAL(3,1),
+	PRIMARY KEY (Fcpf,Pnr));
+	
+-- populando/carregando as tabelas do banco de dados
+insert into FUNCIONARIO values
+	('João','B','Silva','12345678966', '1965-01-09','Rua das flores, 751, São Paulo, SP','M', 30000, '33344555587', 5),
+	('Fernando','T','Wong','33344555587', '1955-12-08','Rua da lapa, 34, Sao Paulo, SP','M', 40000, '88866555576', 5),
+	('Alice','J','Zelaya','99988777767', '1968-01-19','Rua Souza Lima, 35, Curitiba, PR','F', 25000, '98765432168', 4),
+	('Jennifer','S','Souza','98765432168','1941-06-20','Av. Arthur de Lima, 54, Santo Andre, SP','F', 43000, '88866555576', 4),
+	('Ronaldo','K','Lima','66688444476','1962-09-15','Rua Rebouças,65, Piracicaba, SP','M', 38000, '33344555587', 5),
+	('Joice','A','Leite','45345345376','1972-07-31','Av. Lucas Obes, 74,São Paulo, SP','F', 25000, '33344555587', 5),
+	('André','V','Pereira','98798798733','1969-03-29','Rua Timbira, 35,São Paulo, SP','M', 25000, '98765432168', 4),
+    ('Jorge','E','Brito','88866555576','1937-11-10','Rua do Horto, 35,São Paulo, SP','M', 55000, NULL, 1);	
+ insert into DEPARTAMENTO values
+	('Pesquisa',5, '33344555587', '1988-05-22'),
+	('Administração',4, '98765432168', '1995-01-01'),
+	('Matriz',1, '88866555576', '1981-06-19');		
+insert into DEPENDENTE values
+	('33344555587', 'Alicia','F', '1986-04-05', 'Filha'),
+	('33344555587', 'Tiago','M', '1983-10-25', 'Filho'),
+	('33344555587', 'Janaína','F', '1958-05-03', 'Esposa'),
+	('98765432168', 'Antonio','M', '1942-02-28', 'Marido'),
+	('12345678966', 'Michael','M', '1988-01-04', 'Filho'),
+	('12345678966', 'Alice','F', '1988-12-30', 'Filha'),
+	('12345678966', 'Elizabeth','F', '1967-05-05', 'Esposa');	
+insert into LOCALIZACAO_DEP values
+	(1, 'São Paulo'),
+	(4, 'Mauá'),
+	(5, 'Santo André'),
+	(5, 'Itú'),
+	(5, 'São Paulo');	
+insert into TRABALHA_EM values
+	(12345678966,1, 32.5),
+	(12345678966,2, 7.5),
+	(66688444476,3, 40.0),
+	(45345345376,1, 20.0),
+	(45345345376,2, 20.0),
+	(33344555587,2, 10.0),
+	(33344555587,3, 10.0),
+	(33344555587,10, 10.0),
+	(33344555587,20, 10.0),
+	(99988777767,30, 30.0),
+	(99988777767,10, 10.0),
+	(98798798733,10, 35.0),
+	(98798798733,30, 5.0),
+	(98765432168,30, 20.0),
+	(98765432168,20, 15.0),
+	(88866555576,20, NULL);
+insert into PROJETO values
+	('ProdutoX',1, 'Santo André', 5),
+	('ProdutoY',2, 'Itú', 5),
+	('ProdutoZ',3, 'São Paulo', 5),
+	('Informatização',10, 'Mauá', 4),
+	('Reorganização',20, 'São Paulo', 1),
+	('NovosBenefícios',30, 'Mauá', 4);
+
+-- Aplicando a restrição de integridade referencial (chaves estrangeiras - FK)
+alter table FUNCIONARIO	ADD FOREIGN KEY(Dnr) REFERENCES DEPARTAMENTO(Dnumero);
+alter table FUNCIONARIO	ADD FOREIGN KEY(Cpf_supervisor) REFERENCES FUNCIONARIO(Cpf);
+alter table DEPARTAMENTO ADD FOREIGN KEY(Cpf_gerente) REFERENCES FUNCIONARIO(Cpf);
+alter table LOCALIZACAO_DEP	ADD FOREIGN KEY (Dnumero) REFERENCES DEPARTAMENTO(Dnumero);
+alter table TRABALHA_EM	ADD FOREIGN KEY (Fcpf) REFERENCES FUNCIONARIO(Cpf);	
+alter table TRABALHA_EM	ADD FOREIGN KEY(Pnr) REFERENCES PROJETO(Projnumero); 	
+alter table PROJETO	ADD FOREIGN KEY (Dnum) REFERENCES DEPARTAMENTO(Dnumero);
+alter table DEPENDENTE ADD FOREIGN KEY(Fcpf) REFERENCES FUNCIONARIO(CPF);
+commit;
---
+++ Startup_2.sql
@@ -0,0 +1,97 @@
+begin;
+drop schema if exists StartUp_JoseLucas_2;
+create schema StartUp_JoseLucas_2; 
+use StartUp_JoseLucas_2;
+
+-- Criando tabelas
+create table STARTUP(
+    id_startup INT PRIMARY KEY AUTO_INCREMENT,
+    nome_startup VARCHAR(255) NOT NULL,
+    cidade_sede VARCHAR(255)
+);
+
+create table PROGRAMADOR(
+    id_programador INT PRIMARY KEY AUTO_INCREMENT,
+    id_startup INT,
+    nome_programador VARCHAR(255) NOT NULL,
+    genero enum('M','F'),
+    data_nascimento DATE,
+    email varchar(255),
+    UNIQUE(email)
+);
+
+create table PROGRAMADOR_LINGUAGEM(
+	id_programador INT,
+	id_linguagem INT
+);
+
+create table LINGUAGEM_PROGRAMACAO(
+	id_linguagem INT PRIMARY KEY AUTO_INCREMENT,
+	nome_linguagem VARCHAR(255) NOT NULL,
+	ano_lancamento YEAR
+);
+
+create table PROJETOS(
+    id_projetos INT PRIMARY KEY AUTO_INCREMENT,
+    id_startup INT,
+    nome_projeto VARCHAR(255) NOT NULL
+);
+
+-- Populando células
+
+insert into STARTUP values
+	(10001,'Tech4Toy','Porto Alegre'),
+    (10002,'Smart123','Belo Horizonte'),
+    (10003,'knowledgeUp','Rio de Janeiro'),
+    (10004,'BSI Next Level','Recife'),
+    (10005,'QualiHealth','São Paulo'),
+    (10006,'ProEdu','Florianópolis');
+
+insert into PROGRAMADOR values
+	(30001,10001,'João Pedro','M','1993-06-23','joaop@mail.com'),
+    (30002,10002,'Paula Silva','F','1986-01-10','paulas@mail.com'),
+    (30003,10003,'Renata Vieira','F','1991-05-07','renatav@mail.com'),
+	(30004,10004,'Felipe Santos','M','1976-11-25','felipes@mail.com'),
+    (30005,10001,'Ana Cristina','F','1968-02-19','anac@mail.com'),
+    (30006,10004,'Alexandre Alves','M','1988-07-07','alexandrea@mail.com'),
+    (30007,10002,'Laura Marques','F','1987-10-04','lauram@mail.com');
+
+insert into PROGRAMADOR_LINGUAGEM values
+	(30001,20001),
+    (30001,20002),
+    (30002,20003),
+    (30003,20004),
+    (30003,20005),
+    (30004,20005),
+    (30007,20001),
+    (30007,20002);
+
+insert into LINGUAGEM_PROGRAMACAO values
+	(20001,'Python','1991'),
+    (20002,'PHP','1995'),
+    (20003,'Java','1995'),
+    (20004,'C','1972'),
+    (20005,'JavaScript','1995'),
+    (20006,'Dart','2011');
+
+insert into PROJETOS values
+	(40001,10001,'Konzetsu'),
+    (40002,10002,'Piston'),
+    (40003,10003,'EveryDay'),
+    (40004,10004,'PumpIt'),
+    (40005,10005,'BackOffice'),
+    (40006,10006,'Julileu.IO');
+
+-- Aplicando a restrição de integridade referencial (chaves estrangeiras - FK)
+
+alter table PROJETOS ADD FOREIGN KEY(id_startup) REFERENCES STARTUP(id_startup) ON DELETE CASCADE;
+alter table PROGRAMADOR ADD FOREIGN KEY(id_startup) REFERENCES STARTUP(id_startup) ON DELETE CASCADE ON UPDATE CASCADE;
+alter table PROGRAMADOR_LINGUAGEM ADD FOREIGN KEY(id_linguagem) REFERENCES LINGUAGEM_PROGRAMACAO(id_linguagem) ON DELETE RESTRICT;
+alter table PROGRAMADOR_LINGUAGEM ADD FOREIGN KEY(id_programador) REFERENCES PROGRAMADOR(id_programador) ON DELETE RESTRICT;
+
+-- Alterando variável
+
+SET LOCAL lc_time_names = 'pt_BR'; 
+-- SHOW LOCAL VARIABLES like 'lc_time_names'; 
+ 
+commit;
